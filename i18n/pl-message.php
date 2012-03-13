a6e69aa190ac3fb4afaf6af7eb46407ad0fac3d8
marekmurawski <marek.murawski@gmail.com>
2012-03-13 16:45:08 +0100

    Settings contd.

--- i18n/pl-message.php
+++ i18n/pl-message.php
@@ -8,12 +8,6 @@
 return array(
 
 // general
-'Cache administration' => 'Administracja buforowaniem',
-'Cache has been cleared!' => "Bufory zostały wyczyszczone!",
-'Cache has not been cleared!' => "Bufory NIE zostały wyczyszczone!",
-'The button bellow clear all the cache.' => 'Poniższy przycisk wyczyści wszystkie zbuforowane dane',
-'Clear cache' => 'Wyczyść Bufory',
-'Is cacheable' => 'Strona buforowana?',
 'Yes' => 'Tak',
 'No' => 'Nie',
 );
