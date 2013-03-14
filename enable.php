d5c5cc381abb67074a4b397f4f17a69c6106f28b
marekmurawski <marek.murawski@gmail.com>
2013-03-14 09:50:33 +0100

    dev

--- enable.php
+++ enable.php
@@ -1,7 +1,16 @@
 <?php
 
 /* Security measure */
-if ( !defined( 'IN_CMS' ) ) {
+if ( !defined( 'IN_CMS' ) ) 
+    exit();
+
+if ( !Plugin::isEnabled( 'mm_core' ) ) {
+    unset( Plugin::$plugins['mm_cache'] );
+    Plugin::save();
+    Flash::set( 'error', __( '<b>mm_core</b> plugin not found! <br/>
+                            Download latest <b>mm_core</b> here: <br/>
+                            <a href="http://marekmurawski.pl/en/web/wolf-cms/plugins" target="_blank">http://marekmurawski.pl/en/web/wolf-cms/plugins</a>
+                           ' ) );
     exit();
 }
 
